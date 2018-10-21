720bd2f950a3969e1cb0301bdc6b7d694f9a40af
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-10-21 05:34:44 +0000

    finished getBestRoute (TESTED), wrong branch

--- maps/maps.js
+++ maps/maps.js
@@ -1,13 +1,14 @@
 var maps = module.exports = {};
 var https = require('https');
-var request = require('request');
+var request = require('sync-request');
 var key = process.env.GCP_API_Key;
 const gMaps = require('@google/maps').createClient({
     key: key
 });
 
 
-maps.getBestRoute = (from, mids, to) => {
+maps.getBestRoute = (arr) => {
+    var from = arr[0], mids = arr.slice(1, arr.length - 1), to = arr[arr.length - 1];
     var perms = permutations(mids);
     var all = tryall();
     return [from, ...perms[all.indexOf(Math.min(...all))], to];
@@ -26,40 +27,27 @@ maps.getBestRoute = (from, mids, to) => {
             }
         }
         permute(inputArr)
+        console.log(result);
         return result;
     }
     function tryall() {
         let poss = [];
         for (let i in permutations(mids)) {
             poss.push([]);
-            for(let j = 0; j < perms[i].length() - 1; j++) {
+            for(let j = 0; j < perms[i].length - 1; j++) {
                 poss[i].push(routeDuration(perms[i][j], perms[i][j + 1]));
             }
-            poss[i].reduce((a, b) => a + b, routeDuration(from, poss[i][0]) + routeDuration(poss[i][poss[i].length], to));
+            poss[i] = poss[i].reduce((a, b) => a + b, routeDuration(from, perms[i][0]) + routeDuration(perms[i][perms[i].length - 1], to));
         }
         return poss;
     }
-    
 }
 
 function routeDuration(start, end) {
-    gMaps.distanceMatrix({
-        
-    });
-    return getJSON(`http://maps.googleapis.com/maps/api/directions/json?origin=${start}&destination=${start}&key=${key}`);
+    return getJSON(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${start}&destinations=${end}&key=AIzaSyAW8vHOPnGkiK9VbfD5657OTynlenNCBVk`).rows[0].elements[0].duration.value;
 }
 
 function getJSON(url) {
-    var data;
-    https.get(url, function(res){
-        res.on('data', function(chunk){
-            data += chunk;
-        });
-        res.on('end', function(){
-            var data = JSON.parse(data);
-        });
-    }).on('error', function(e){
-    }).end();
-    return data;
+    return JSON.parse(request('GET', url).getBody('utf8'));
 }
-console.log(getJSON('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington,DC&destinations=New+York+City,NY%20Disneyland&key=AIzaSyCZMNFyJg6qmMh6MVw-ez_5BNBGVv9fwk8'));
+console.log(maps.getBestRoute(['San+Francisco,+CA', 'Orinda,+CA', 'Piedmont,+CA', 'Pacifica,+CA', 'Palo+Alto,+CA']));
