47157a5a73376a8a9426e27d0061ac5d7fc9a505
natalielim <natalielim@gmail.com>
2018-10-20 19:27:24 -0700

    adding trip element
    added index and new page

--- models/trip.js
+++ models/trip.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TripSchema = new Schema({
-  title: { type: String, required: true }
+  title: { type: String, required: true },
+  users: [{ type: Schema.Types.ObjectId, ref: 'User'}],
 });
 
 const Trip = mongoose.model('Trip', TripSchema);
--- routes/trips.js
+++ routes/trips.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const auth = require('./helpers/auth');
-const Room = require('../models/trip');
+const Trip = require('../models/trip');
 
 const router = express.Router();
+
+// Trips index
+router.get('/', (req, res) => {
+  Trip.find({}, 'title', (err, trips) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.render('trips/index', { trips: trips });
+    }
+  });
+});
 
 // Trips new
 router.get('/new', auth.requireLogin, (req, res, next) => {
@@ -14,25 +25,14 @@ router.get('/:id', auth.requireLogin, (req, res, next) => {
   // TODO
 });
 
-// // Trips create
-// router.post('/', auth.requireLogin, (req, res, next) => {
-//   let trip = new Trip(req.body);
-//
-//   trip.save(function(err, trip) {
-//     if(err) { console.error(err) };
-//
-//     return res.redirect('/');
-//   });
-// });
-
 // Trips create
-router.post('/', auth.requireLogin, (req, res, next) => {
+router.post('/', auth.requireLogin, (req, res) => {
   let trip = new Trip(req.body);
 
-  trip.save(function(err, trip) {
-    if(err) console.log(err);
-    return res.redirect('/new');
+  trip.save((err, folder) => {
+    if (err) { console.error(err); }
+    return res.redirect('/trips');
   });
-})
+});
 
 module.exports = router;
